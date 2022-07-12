4ec04a6069590670e5068cdb87f8b45723f9068c
Joel Crawford <macbook@Macbooks-MacBook-Pro.local>
2022-07-12 15:26:56 +0300

    Model

---
+++ Camera App/Model/MediaObject.swift
@@ -0,0 +1,17 @@
+//
+//  MediaObject.swift
+//  Camera App
+//
+//  Created by Joel Crawford on 7/12/22.
+//
+
+import Foundation
+
+struct MediaObject {
+    let imagedata: Data?
+    let videoURL: String?
+    let caption: String?
+    let id = UUID().uuidString
+    let createDate = Date()
+    let endDate:Date?
+}
